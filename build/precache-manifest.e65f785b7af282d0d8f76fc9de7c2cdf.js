4f02de70334249d1dc0da7cf6d37a70ed1fad803
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2020-04-04 00:48:02 +0530

    deploying with build

---
+++ build/precache-manifest.e65f785b7af282d0d8f76fc9de7c2cdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c2cf817c5a5334673de",
+    "url": "/static/js/main.8c2cf817.chunk.js"
+  },
+  {
+    "revision": "45ea534042495dbb2393",
+    "url": "/static/js/1.45ea5340.chunk.js"
+  },
+  {
+    "revision": "8c2cf817c5a5334673de",
+    "url": "/static/css/main.14cadef9.chunk.css"
+  },
+  {
+    "revision": "c84d7768c359838ab4f7a3e63267dbdf",
+    "url": "/index.html"
+  }
+];
